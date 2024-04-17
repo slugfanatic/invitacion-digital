6d95032b6fc68faad94d7c41e14be890f17fe29b
slugfanatic <slugfanatic@gmail.com>
2024-04-16 23:01:55 -0600

    a

--- src/Themes/Shared/TypographyBase.ts
+++ src/Themes/Shared/TypographyBase.ts
@@ -5,52 +5,52 @@ const TypographyBase = createTheme({
     h1: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 2px #000",
     },
     h2: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 20px #000",
     },
     h3: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 5px #000",
     },
     h4: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 5px #000",
     },
     h5: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 5px #000",
     },
     h6: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 5px #000",
     },
     subtitle1: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 5px #000",
     },
     subtitle2: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 5px #000",
     },
     body1: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 5px #000",
     },
     body2: {
       textAlign: "center",
       fontWeight: "unset",
-      textShadow: "0px 0px 10px #000",
+      textShadow: "0px 0px 5px #000",
     },
   },
 });
