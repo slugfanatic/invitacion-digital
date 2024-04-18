e33050df5cdda79796ad1475f0fce934b226f380
slugfanatic <slugfanatic@gmail.com>
2024-04-17 23:34:01 -0600

    a

--- src/Components/Labels/LblTimeItemString/LblTimeItemString.tsx
+++ src/Components/Labels/LblTimeItemString/LblTimeItemString.tsx
@@ -2,7 +2,11 @@ import { Typography } from "@mui/material";
 import IChildrenProp from "../../../Interfaces/Base/IChildrenProp";
 
 const LblTimeItemString: React.FC<IChildrenProp> = ({ children }) => {
-  return <Typography variant="body1" color="text.primary">{children}</Typography>;
+  return (
+    <Typography variant="body2" color="text.primary">
+      {children}
+    </Typography>
+  );
 };
 
 export default LblTimeItemString;
