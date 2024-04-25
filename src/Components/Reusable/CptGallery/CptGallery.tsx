ef9ad3d53fd785d356750b6824f8113ce6566944
slugfanatic <slugfanatic@gmail.com>
2024-04-25 17:35:09 -0600

    a

--- src/Components/Reusable/CptGallery/CptGallery.tsx
+++ src/Components/Reusable/CptGallery/CptGallery.tsx
@@ -1,6 +1,7 @@
-import { Stack, Theme, useMediaQuery } from "@mui/material";
 import { useEffect, useState } from "react";
+import { Stack } from "@mui/material";
 import { useCelebrationContext } from "../../../Context/CelebrationContext";
+import useBreakpoints from "../../../Hooks/useBreakpoints";
 import CptImageHolder from "../CptImageHolder/CptImageHolder";
 import IImageGallery from "../../../Interfaces/Components/IImageGallery";
 import { chunkArray } from "../../../Utils/chunkArray";
@@ -9,12 +10,9 @@ const CptGallery: React.FC = (): JSX.Element => {
   const {
     currentCelebrant: { imageGallery },
   } = useCelebrationContext();
-
+  const { smUp, mdUp } = useBreakpoints();
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [columns, setColumns] = useState(1);
-
-  const mdUp = useMediaQuery((theme: Theme) => theme.breakpoints.up("md"));
-  const smUp = useMediaQuery((theme: Theme) => theme.breakpoints.up("sm"));
 
   const actualizarAnchoNavegador = () => {
     setWindowWidth(window.innerWidth);
