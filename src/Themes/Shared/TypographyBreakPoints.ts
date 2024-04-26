50ea5fb8959053eb17ff7c1ae1e5d5ca6f88163d
slugfanatic <slugfanatic@gmail.com>
2024-04-25 19:00:40 -0600

    a

--- src/Themes/Shared/TypographyBreakPoints.ts
+++ src/Themes/Shared/TypographyBreakPoints.ts
@@ -3,153 +3,153 @@ import { createTheme } from "@mui/material";
 const TypographyBreakPoints = createTheme({
   typography: {
     h1: {
-      fontSize: "4.5rem",
+      fontSize: "4.95rem",
       "@media (min-width:600px)": {
-        fontSize: "5rem",
-      },
-      "@media (min-width:900px)": {
         fontSize: "5.5rem",
       },
+      "@media (min-width:900px)": {
+        fontSize: "6.05rem",
+      },
       "@media (min-width:1200px)": {
-        fontSize: "6rem",
+        fontSize: "6.6rem",
       },
       "@media (min-width:1536px)": {
-        fontSize: "7rem",
+        fontSize: "7.7rem",
       },
     },
     h2: {
-      fontSize: "4rem",
+      fontSize: "4.4rem",
       "@media (min-width:600px)": {
-        fontSize: "4.5rem",
+        fontSize: "4.95rem",
       },
       "@media (min-width:900px)": {
-        fontSize: "5rem",
-      },
-      "@media (min-width:1200px)": {
         fontSize: "5.5rem",
       },
+      "@media (min-width:1200px)": {
+        fontSize: "6.05rem",
+      },
       "@media (min-width:1536px)": {
-        fontSize: "6rem",
+        fontSize: "6.6rem",
       },
     },
     h3: {
-      fontSize: "3.5rem",
+      fontSize: "3.85rem",
       "@media (min-width:600px)": {
-        fontSize: "4rem",
+        fontSize: "4.4rem",
       },
       "@media (min-width:900px)": {
-        fontSize: "4.5rem",
+        fontSize: "4.95rem",
       },
       "@media (min-width:1200px)": {
-        fontSize: "5rem",
+        fontSize: "5.5rem",
       },
       "@media (min-width:1536px)": {
-        fontSize: "5.5rem",
+        fontSize: "6.05rem",
       },
     },
     h4: {
-      fontSize: "2.25rem",
+      fontSize: "2.475rem",
       "@media (min-width:600px)": {
-        fontSize: "2.5rem",
-      },
-      "@media (min-width:900px)": {
         fontSize: "2.75rem",
       },
+      "@media (min-width:900px)": {
+        fontSize: "3.025rem",
+      },
       "@media (min-width:1200px)": {
-        fontSize: "3rem",
+        fontSize: "3.3rem",
       },
       "@media (min-width:1536px)": {
-        fontSize: "3.25rem",
+        fontSize: "3.575rem",
       },
     },
     h5: {
-      fontSize: "2rem",
+      fontSize: "2.2rem",
       "@media (min-width:600px)": {
-        fontSize: "2.25rem",
+        fontSize: "2.475rem",
       },
       "@media (min-width:900px)": {
-        fontSize: "2.5rem",
-      },
-      "@media (min-width:1200px)": {
         fontSize: "2.75rem",
       },
+      "@media (min-width:1200px)": {
+        fontSize: "3.025rem",
+      },
       "@media (min-width:1536px)": {
-        fontSize: "3rem",
+        fontSize: "3.3rem",
       },
     },
     h6: {
-      fontSize: "1.375rem",
+      fontSize: "1.5125rem",
       "@media (min-width:600px)": {
-        fontSize: "1.5rem",
+        fontSize: "1.65rem",
       },
       "@media (min-width:900px)": {
-        fontSize: "1.625rem",
+        fontSize: "1.7875rem",
       },
       "@media (min-width:1200px)": {
-        fontSize: "1.75rem",
+        fontSize: "1.925rem",
       },
       "@media (min-width:1536px)": {
-        fontSize: "1.875rem",
+        fontSize: "2.0625rem",
       },
     },
     subtitle1: {
-      fontSize: "1.125rem",
+      fontSize: "1.2375rem",
       "@media (min-width:600px)": {
-        fontSize: "1.25rem",
-      },
-      "@media (min-width:900px)": {
         fontSize: "1.375rem",
       },
+      "@media (min-width:900px)": {
+        fontSize: "1.5125rem",
+      },
       "@media (min-width:1200px)": {
-        fontSize: "1.5rem",
+        fontSize: "1.65rem",
       },
       "@media (min-width:1536px)": {
-        fontSize: "1.625rem",
+        fontSize: "1.7875rem",
       },
     },
     subtitle2: {
-      fontSize: "1rem",
+      fontSize: "1.1rem",
       "@media (min-width:600px)": {
-        fontSize: "1.125rem",
+        fontSize: "1.2375rem",
       },
       "@media (min-width:900px)": {
-        fontSize: "1.25rem",
-      },
-      "@media (min-width:1200px)": {
         fontSize: "1.375rem",
       },
+      "@media (min-width:1200px)": {
+        fontSize: "1.5125rem",
+      },
       "@media (min-width:1536px)": {
-        fontSize: "1.5rem",
+        fontSize: "1.65rem",
       },
     },
     body1: {
-      fontSize: "1.25rem",
+      fontSize: "1.375rem",
       "@media (min-width:600px)": {
-        fontSize: "1.375rem",
+        fontSize: "1.5125rem",
       },
       "@media (min-width:900px)": {
-        fontSize: "1.5rem",
+        fontSize: "1.65rem",
       },
       "@media (min-width:1200px)": {
-        fontSize: "1.625rem",
+        fontSize: "1.7875rem",
       },
       "@media (min-width:1536px)": {
-        fontSize: "1.75rem",
+        fontSize: "1.925rem",
       },
     },
     body2: {
-      fontSize: "1.125rem",
+      fontSize: "1.2375rem",
       "@media (min-width:600px)": {
-        fontSize: "1.25rem",
-      },
-      "@media (min-width:900px)": {
         fontSize: "1.375rem",
       },
+      "@media (min-width:900px)": {
+        fontSize: "1.5125rem",
+      },
       "@media (min-width:1200px)": {
-        fontSize: "1.5rem",
+        fontSize: "1.65rem",
       },
       "@media (min-width:1536px)": {
-        fontSize: "1.625rem",
+        fontSize: "1.7875rem",
       },
     },
   },
