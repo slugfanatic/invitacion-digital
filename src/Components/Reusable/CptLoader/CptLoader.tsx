189bcb6217093b118943fd99144230fd3d1614a0
slugfanatic <slugfanatic@gmail.com>
2024-04-27 22:10:42 -0600

    a

--- src/Components/Reusable/CptLoader/CptLoader.tsx
+++ src/Components/Reusable/CptLoader/CptLoader.tsx
@@ -3,44 +3,59 @@ import { Box, Button, Fade } from "@mui/material";
 
 const TopEnvelop: React.FC = (): JSX.Element => {
   const screenWidth = window.innerWidth;
-  const screenHeight = window.innerHeight * 0.6;
+  const svgHeight = window.innerHeight * 0.6;
 
-  const points = `0,0 0,${screenHeight * 0.5} ${
-    screenWidth * 0.5
-  },${screenHeight} ${screenWidth},${screenHeight * 0.5} ${screenWidth},${0} `;
+  const points =
+    `0,0` +
+    ` ` +
+    `0,${svgHeight * 0.4}` +
+    ` ` +
+    `${screenWidth * 0.5},${svgHeight}` +
+    ` ` +
+    `${screenWidth},${svgHeight * 0.4}` +
+    ` ` +
+    `${screenWidth},${0}`;
 
   return (
     <svg
       width={screenWidth}
-      height={screenHeight}
+      height={svgHeight}
       style={{
         position: "fixed",
         top: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
+        left: 0,
       }}
     >
       <defs>
         <linearGradient id="grad" x1="100%" y1="100%" x2="100%" y2="0%">
           <stop offset="0%" style={{ stopColor: "#fff", stopOpacity: 1 }} />
-          <stop offset="100%" style={{ stopColor: "#000", stopOpacity: 1 }} />
+          <stop offset="100%" style={{ stopColor: "#ddd", stopOpacity: 1 }} />
         </linearGradient>
       </defs>
-      <polygon points={points} fill="url(#grad)" />
+      <polygon points={points} fill="url(#grad)" fillOpacity="0.6" />
     </svg>
   );
 };
 
 const BottomEnvelop: React.FC = (): JSX.Element => {
+  const screenWidth = window.innerWidth;
+  const svgHeight = window.innerHeight * 0.65;
+
+  const points =
+    `0,${svgHeight}` +
+    ` ` +
+    `${screenWidth * 0.5},0` +
+    ` ` +
+    `${screenWidth},${svgHeight}`;
+
   return (
     <svg
-      viewBox="0 0 100 50"
+      width={screenWidth}
+      height={svgHeight}
       style={{
         position: "fixed",
         bottom: 0,
         left: 0,
-        width: "100%",
-        height: "auto",
       }}
     >
       <defs>
@@ -49,7 +64,7 @@ const BottomEnvelop: React.FC = (): JSX.Element => {
           <stop offset="100%" style={{ stopColor: "#fff", stopOpacity: 1 }} />
         </linearGradient>
       </defs>
-      <polygon points="0,0 20,20 80,20 100,0 100,50 0,50" fill="url(#grad)" />
+      <polygon points={points} fill="url(#grad)" fillOpacity="0.8" />
     </svg>
   );
 };
